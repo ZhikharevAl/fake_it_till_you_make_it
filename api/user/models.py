32b5f21c20d8a003c9c12d1c78f3fdbeba7ec10d
ZhikharevAl <waltafunk@gmail.com>
2025-04-11 01:41:43 +0000

    feat: added pydantic model AddToFavouritesPayload

--- api/user/models.py
+++ api/user/models.py
@@ -1,1 +1,10 @@
+from pydantic import BaseModel, Field
+
+
+class AddToFavouritesPayload(BaseModel):
+    """Модель тела запроса для POST /api/user/favourites."""
+
+    request_id: str = Field(..., min_length=1, description="ID запроса для добавления в избранное")
+
+
 FavouritesListResponse = list[str]
