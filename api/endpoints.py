c712f39f38a6e4a428e347ba19463d3fd29b4596
ZhikharevAl <waltafunk@gmail.com>
2025-04-14 19:17:53 +0000

    feat: added USER_FAVOURITES_DETAIL

--- api/endpoints.py
+++ api/endpoints.py
@@ -9,6 +9,7 @@ class APIEndpoints(Enum):
     AUTH = f"{API_PREFIX}/auth"
     USER = f"{API_PREFIX}/user"
     USER_FAVOURITES = f"{API_PREFIX}/user/favourites"
+    USER_FAVOURITES_DETAIL = f"{USER_FAVOURITES}/{{requestId}}"
 
     def format(self, **kwargs: str) -> str:
         """Форматирует URL эндпоинта, подставляя значения для path-параметров."""
