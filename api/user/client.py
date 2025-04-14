acf66951c1ec879b8f847fa1c2a6e3a83fae724e
ZhikharevAl <waltafunk@gmail.com>
2025-04-14 20:13:10 +0000

    feat: added remove from davourites method

--- api/user/client.py
+++ api/user/client.py
@@ -47,7 +47,7 @@ class UserClient(BaseAPI):
                 return validated_list
         return processed_response
 
-    @allure.step("Добавление запроса в избранное:")
+    @allure.step("Добавление запроса в избранное")
     def add_to_favourites(
         self, payload: AddToFavouritesPayload, expected_status: int = 200
     ) -> APIResponse:
@@ -59,6 +59,25 @@ class UserClient(BaseAPI):
         endpoint = APIEndpoints.USER_FAVOURITES
         logger.info("Вызов POST %s c payload: %s", endpoint.value, payload)
         response = self.http.post(endpoint=endpoint.value, json=payload.model_dump(by_alias=True))
+        processed_response = self._handle_response(response, expected_status)
+        if response.status == 200 and expected_status == 200:
+            allure.attach(
+                name="Тело ответа (200 OK, text/plain)",
+                body=response.text(),
+                attachment_type=allure.attachment_type.TEXT,
+            )
+        return processed_response
+
+    @allure.step("Удаление запроса из избранного")
+    def remove_from_favourites(self, request_id: str, expected_status: int = 200) -> APIResponse:
+        """
+        Выполняет DELETE /api/user/favourites/{requestId}. Требует аутентификации.
+
+        Возвращает APIResponse. Тело при успехе (200) - text/plain.
+        """
+        endpoint = APIEndpoints.USER_FAVOURITES_DETAIL.format(requestId=request_id)
+        logger.info("Вызов DELETE %s", endpoint)
+        response = self.http.delete(endpoint=endpoint)
         processed_response = self._handle_response(response, expected_status)
         if response.status == 200 and expected_status == 200:
             allure.attach(
