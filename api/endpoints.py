243426a94ea0d70719c6200403dc4263bef004b6
ZhikharevAl <waltafunk@gmail.com>
2025-04-16 18:33:15 +0000

    feat: added endpoints requests

--- api/endpoints.py
+++ api/endpoints.py
@@ -7,9 +7,14 @@ class APIEndpoints(Enum):
     """Класс для хранения эндпоинтов API."""
 
     AUTH = f"{API_PREFIX}/auth"
+
     USER = f"{API_PREFIX}/user"
     USER_FAVOURITES = f"{API_PREFIX}/user/favourites"
     USER_FAVOURITES_DETAIL = f"{USER_FAVOURITES}/{{requestId}}"
+
+    REQUESTS = f"{API_PREFIX}/request"
+    REQUEST_DETAIL = f"{REQUESTS}/{{id}}"
+    REQUEST_CONTRIBUTION = f"{REQUEST_DETAIL}/contribution"
 
     def format(self, **kwargs: str) -> str:
         """Форматирует URL эндпоинта, подставляя значения для path-параметров."""
