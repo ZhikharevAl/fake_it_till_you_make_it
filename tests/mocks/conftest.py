f30b0f4068a29bd57650e9037a2ddb158134f53d
ZhikharevAl <waltafunk@email.com>
2025-05-04 23:08:51 +0300

    feat: add mock_request_client fixture for RequestClient in tests

--- tests/mocks/conftest.py
+++ tests/mocks/conftest.py
@@ -5,6 +5,7 @@ from typing import Any
 import pytest
 
 from api.auth.client import AuthClient
+from api.request.client import RequestClient
 from api.user.client import UserClient
 from core.mock_http_client import MockHTTPClient
 from utils.mock_factory import MockFactory, MockHTTPClientProtocol
@@ -36,3 +37,9 @@ def mock_auth_client(mock_http_client: MockHTTPClient) -> AuthClient:
 def mock_user_client(mock_http_client: MockHTTPClient) -> UserClient:
     """Предоставляет экземпляр UserClient, использующий MockHTTPClient."""
     return UserClient(http_client=mock_http_client)
+
+
+@pytest.fixture
+def mock_request_client(mock_http_client: MockHTTPClient) -> RequestClient:
+    """Предоставляет экземпляр RequestClient, использующий MockHTTPClient."""
+    return RequestClient(http_client=mock_http_client)
